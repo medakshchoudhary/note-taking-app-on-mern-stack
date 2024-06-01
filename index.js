e94a85a080de40f046e8abf184c1068e435e79c2
medakshchoudhary <ch.daksh01@gmail.com>
2024-06-01 23:54:55 +0530

    created add a note and show it on the screen feature

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const { log } = require('console');
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}));
@@ -8,11 +10,15 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
-    res.render("index");
+    fs.readdir(`./files`,function(err,files){
+        res.render("index",{files: files});
+    });
 });
 
 app.post("/create", function(req,res){
-    
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description,function(err){
+        res.redirect("/");
+    });
 })
 
 
